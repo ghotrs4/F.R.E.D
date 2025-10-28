9f294fbcabebe7e92cec8a59a98b5b6528ad1968
cfunn <charlotte.funnekotter@gmail.com>
2025-10-28 16:23:14 -0400

    intial UI setup

---
+++ UI/src/components/FilterItems.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p>Filter Items</p>
+    <button type="button" @click="count++">All</button>
+    <button type="button" @click="count++">Expiring Soon</button>
+    <button type="button" @click="count++">Expired</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
